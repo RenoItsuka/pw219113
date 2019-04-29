e5917be1b4e841e18a4148f546e8e1d0f76f8d3f
RenoItsuka <renatomascareno@gmail.com>
2019-04-29 13:59:51 -0600

    Conexion a la base de datos

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const path = require('path');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 const app = express();
 
 //importamos las rutas a traves de una constante
 const indiceRutas=require('./rutas/index');
 
+//Configuraciones 
+app.set('port', process.env.PORT || 3000);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+//Middeleware
+app.use(myConnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database:'crudenodemysql13'
+},'single'));
+
 // Usar las Rutas
 app.use("/" , indiceRutas);
 
 //Servidor que este escuchando 
-app.listen(3000, () => {
+app.listen(app.get('port'), () => {
     console.log("Servidor escuchando puerto 3000");
 } )
